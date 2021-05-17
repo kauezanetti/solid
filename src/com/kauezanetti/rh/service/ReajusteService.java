a07e6d990688efc09ccf415104f206b5b9354773
Kauê Gustavo Zanetti <kauezanetti@hotmail.com>
2021-05-17 00:16:43 -0300

    extraindo as regras de reajuste salarial e implementar utilizando extensão para reduzir o acoplamento, seguindo o principio Open Closed Principle

---
+++ src/com/kauezanetti/rh/service/ReajusteService.java
@@ -0,0 +1,21 @@
+package com.kauezanetti.rh.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import com.kauezanetti.rh.model.Funcionario;
+
+public class ReajusteService {
+	
+	private List<ValidacaoReajuste> validacoes;
+	
+	public void reajusteService(List<ValidacaoReajuste> validacoes) {
+		this.validacoes = validacoes;
+	}
+	
+	public void validacao(Funcionario funcionario, BigDecimal aumento) {
+		this.validacoes.forEach(v -> v.validar(funcionario, aumento));
+		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
+		funcionario.atualizarSalario(salarioReajustado);
+	}
+}
--- src/com/kauezanetti/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
+++ src/com/kauezanetti/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -1,31 +1,19 @@
 package com.kauezanetti.rh.service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 import com.kauezanetti.rh.ValidacaoException;
 import com.kauezanetti.rh.model.Funcionario;
 
-public class ReajusteSalarialService {
-	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste{
+	public void validar(Funcionario funcionario, BigDecimal aumento) {		
 		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
 		LocalDate dataAtual = LocalDate.now();
-		
 		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-		
 		if (mesesDesdeUltimoReajuste > 6) {
 			throw new ValidacaoException("Intervalor entre reajuste salarial deve ser de no minimo 6 meses!");
 		}
-		
-		funcionario.setSalario(salarioAtual.add(aumento));
-		funcionario.setDataUltimoReajuste(LocalDate.now());
 	}
 }
