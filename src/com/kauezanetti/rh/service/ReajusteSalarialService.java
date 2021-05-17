13774d0017bef1f678b5bf90f220cd06cb90088b
Kauê Gustavo Zanetti <kauezanetti@hotmail.com>
2021-05-16 23:42:53 -0300

    Implementação da regra de reajuste salarial, onde o empregado devera ser elegível a receber reajuste salário após o tempo mínimo de 6 meses do ultimo reajuste

--- src/com/kauezanetti/rh/service/ReajusteSalarialService.java
+++ src/com/kauezanetti/rh/service/ReajusteSalarialService.java
@@ -3,6 +3,7 @@ package com.kauezanetti.rh.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import com.kauezanetti.rh.ValidacaoException;
 import com.kauezanetti.rh.model.Funcionario;
@@ -14,6 +15,16 @@ public class ReajusteSalarialService {
 		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
 			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
 		}
+		
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		
+		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		
+		if (mesesDesdeUltimoReajuste > 6) {
+			throw new ValidacaoException("Intervalor entre reajuste salarial deve ser de no minimo 6 meses!");
+		}
+		
 		funcionario.setSalario(salarioAtual.add(aumento));
 		funcionario.setDataUltimoReajuste(LocalDate.now());
 	}
